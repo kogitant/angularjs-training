f97b714d493fa1ac3e8efade056393867db50833
trautonen <trautonen@github.com>
2013-09-04 00:51:01 +0300

    Simple CORS handler interceptor.

--- score-tables-rest-springmvc/src/main/java/org/eluder/score/tables/rest/springmvc/configuration/SpringMvcConfiguration.java
+++ score-tables-rest-springmvc/src/main/java/org/eluder/score/tables/rest/springmvc/configuration/SpringMvcConfiguration.java
@@ -2,6 +2,7 @@ package org.eluder.score.tables.rest.springmvc.configuration;
 
 import java.util.List;
 
+import org.eluder.score.tables.rest.springmvc.interceptor.CorsHandlerInterceptor;
 import org.eluder.score.tables.service.configuration.ServiceConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -30,9 +32,18 @@ public class SpringMvcConfiguration extends WebMvcConfigurerAdapter {
         return converter;
     }
     
+    @Bean
+    public CorsHandlerInterceptor corsHandler() {
+        return new CorsHandlerInterceptor();
+    }
+    
     @Override
     public void configureMessageConverters(final List<HttpMessageConverter<?>> converters) {
         converters.add(jsonConverter());
     }
     
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(corsHandler());
+    }
 }
