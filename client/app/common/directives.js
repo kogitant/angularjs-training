6f1b5440ed9bcc2614d3fac34fb37afb4d218fd4
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2014-03-06 11:22:26 +0200

    directives moved in their own module and example directives added

--- client/app/common/directives.js
+++ client/app/common/directives.js
@@ -1,29 +1,9 @@
 'use strict';
-var kikkeriApp; // from app.js
 
-kikkeriApp.directive('matchResult', function() {
-	// Runs during compile
-	return {
-		// name: '',
-		// priority: 1,
-		// terminal: true,
-		// scope: {}, // {} = isolate, true = child, false/undefined = no change
-		// controller: function($scope, $element, $attrs, $transclude) {},
-		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-		template: '<h1>{{match.bluePlayerName}}</h1>',
-		// templateUrl: '../views/match-result.html',
-		replace: true,
-		// transclude: true,
-		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-		link: function($scope, iElm, iAttrs, controller) {			
-			$scope.match = iAttrs.match;
-		}
-	};
-});
-app.mod ule
+var kikkeriDirectives = angular.module('kikkeriDirectives', []);
+
 // https://github.com/kseamon/fast-bind/blob/master/src/directives/bind-once/bind-once.js
-app.directive('fastBindOnce', ['$parse', function($parse) {
+kikkeriDirectives.directive('fastBindOnce', ['$parse', function($parse) {
 	return {
 		compile: function compile(element, attributes) {
 			var expr = $parse(attributes.fastBindOnce);
@@ -34,3 +14,25 @@ app.directive('fastBindOnce', ['$parse', function($parse) {
 		}
 	};
 }]);
+
+
+kikkeriDirectives.directive('centralize', function() {
+	return {
+		restrict: 'A',
+		link: function($scope, element, attrs) {
+
+			var elemWidth = element.context.offsetWidth;
+			var elemHeight = element.context.offsetHeight;
+			var top = Math.max(0, (($(window).height() - elemHeight) / 2) + $(window).scrollTop());
+			var left = Math.max(0, (($(window).width() - elemWidth) / 2) + $(window).scrollLeft());
+			$(element).css({
+				position: 'absolute',
+				'z-index': 1000,
+				margin:0,
+				top: (top > 0 ? top : 0)+'px',
+				left: (left > 0 ? left : 0)+'px'
+			});
+
+		}
+	}
+});
