4d7146b7bd533c3c7ff326b8071522af744269f9
jsalonen <jaakko.salonen@iki.fi>
2015-03-13 11:08:10 +0200

    Remove all security deps

--- src/main/java/funmarket/FunmarketApplication.java
+++ src/main/java/funmarket/FunmarketApplication.java
@@ -2,8 +2,6 @@ package funmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
 
 @SpringBootApplication
 public class FunmarketApplication {
@@ -12,23 +10,3 @@ public class FunmarketApplication {
     }
 }
 
-@Configuration
-class WebSecurityConfig {
-
-    /*
-    @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
-        return new AuthenticationManagerBuilder(new NopPostProcessor())
-            .inMemoryAuthentication().withUser("user").password("password").roles("USER")
-            .and().and().build();
-    }
-    */
-
-    private static class NopPostProcessor implements ObjectPostProcessor {
-        @Override
-        @SuppressWarnings("unchecked")
-        public Object postProcess(Object object) {
-            return object;
-        }
-    };
-}
