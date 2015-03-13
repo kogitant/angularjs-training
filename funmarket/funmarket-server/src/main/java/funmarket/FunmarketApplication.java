e0ff36e0095de6e4f89b908821fb8741b8b5180c
jsalonen <jaakko.salonen@iki.fi>
2015-03-13 15:39:35 +0200

    funmarket-server: added validations

--- funmarket/funmarket-server/src/main/java/funmarket/FunmarketApplication.java
+++ funmarket/funmarket-server/src/main/java/funmarket/FunmarketApplication.java
@@ -2,11 +2,24 @@ package funmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class FunmarketApplication {
     public static void main(String[] args) {
         SpringApplication.run(FunmarketApplication.class, args);
+    }
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
     }
 }
 
--- funmarket/funmarket-server/src/main/java/funmarket/marketad/MarketAd.java
+++ funmarket/funmarket-server/src/main/java/funmarket/marketad/MarketAd.java
@@ -1,15 +1,31 @@
 package funmarket.marketad;
 
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Document(collection = "marketads")
 public class MarketAd {
     @Id
     private String id;
+
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String description;
+
+    @NotNull
+    @Min(5)
+    @Max(100000)
     private Integer priceCents;
+
+    private String email;
+    private String phone;
 
     public MarketAd() {
     }
@@ -27,6 +43,10 @@ public class MarketAd {
     }
 
     public Integer getPriceCents() { return priceCents; }
+
+    public String getEmail() {return email; }
+
+    public String getPhone() {return phone; }
 
     @Override
     public String toString() {
