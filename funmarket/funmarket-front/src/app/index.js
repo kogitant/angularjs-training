15c748d65b92a14b67dc9fd2da58f75a396b9c2f
jsalonen <jaakko.salonen@iki.fi>
2015-03-15 18:13:40 +0200

    funmarket: fix api urls in client

--- funmarket/funmarket-front/src/app/index.js
+++ funmarket/funmarket-front/src/app/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('front', ['ngResource', 'ngRoute', 'angularFileUpload'])
-  .value('funmarketApiUrl', 'http://localhost:8080') //funmarket-api.herokuapp.com')
+  .value('funmarketApiUrl', 'http://funmarket-api.herokuapp.com')
+  //.value('funmarketApiUrl', 'http://localhost:8080')
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
