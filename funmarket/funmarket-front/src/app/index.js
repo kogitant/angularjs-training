10679bf39973ca658868c010880f78417015890b
Marko Rautajoki <marko.rautajoki@gofore.com>
2015-03-18 15:42:56 +0200

    Added some localizations

--- funmarket/funmarket-front/src/app/index.js
+++ funmarket/funmarket-front/src/app/index.js
@@ -1,9 +1,10 @@
 'use strict';
 
-angular.module('front', ['ngResource', 'ngRoute', 'angularFileUpload'])
+angular.module('front', ['ngResource', 'ngRoute', 'angularFileUpload', 'pascalprecht.translate'])
   .value('funmarketApiUrl', 'http://funmarket-api.herokuapp.com')
+  .constant('availableLanguages', ['fi', 'en'])
   //.value('funmarketApiUrl', 'http://localhost:8080')
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $translateProvider, availableLanguages) {
     $routeProvider
       .when('/', {
         templateUrl: 'app/main/main.html',
@@ -16,4 +17,20 @@ angular.module('front', ['ngResource', 'ngRoute', 'angularFileUpload'])
       .otherwise({
         redirectTo: '/'
       });
+
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'locale/locale-',
+        suffix: '.json'
+    });
+    $translateProvider.preferredLanguage(availableLanguages[0]);
+  })
+  .controller('localeController', function($scope, $translate, availableLanguages){
+
+        $scope.availableLanguages = availableLanguages;
+        $scope.currentLanguage = $translate.proposedLanguage();
+
+        $scope.changeLang = function(lang) {
+            $translate.use(lang);
+            $scope.currentLanguage = lang;
+        };
   });
